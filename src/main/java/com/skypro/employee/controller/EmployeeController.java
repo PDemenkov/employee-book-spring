134b407ec643c4ac5397698fa948c3b78a29595c
PDemenkov <guapnik@gmail.com>
2022-11-13 00:14:01 +0300

    Проверить 3 GET запроса, MIN MAX High-salary

---
+++ src/main/java/com/skypro/employee/controller/EmployeeController.java
@@ -0,0 +1,60 @@
+package com.skypro.employee.controller;
+
+import com.skypro.employee.model.Employee;
+import com.skypro.employee.record.EmployeeRequest;
+import com.skypro.employee.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Http методы
+ * GET - получение ресурса или набора ресурсов
+ * POST - создание ресурса
+ * PUT - Модификация ресурса (изменить все поля)
+ * PATCH - частичная модификация
+ * - DELETE - удаление ресурса
+ */
+@RestController
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/employees")
+    public Collection<Employee> getAllEmployees() {
+        return this.employeeService.getAllEmployees();
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody EmployeeRequest employeeRequest) {
+        return this.employeeService.addEmployee(employeeRequest);
+    }
+
+    @GetMapping("/employees/salary/sum")
+    public int getSalarySum() {
+        return this.employeeService.getSalarySum();
+    }
+
+    @GetMapping("/employees/salary/max")
+    public int getSalaryMax() {
+        return this.employeeService.getSalaryMax();
+    }
+
+    @GetMapping("employees/salary/min")
+    public int getSalaryMin() {
+        return this.employeeService.getSalaryMin();
+    }
+
+    @GetMapping("employees/salary/high-salary")
+    public Collection<Employee> getEmployeeBiggerThanAverage(){
+        return this.employeeService.getEmployeeBiggerThanAverage();
+    };
+}
---
+++ src/main/java/com/skypro/employee/service/EmployeeService.java
@@ -0,0 +1,57 @@
+package com.skypro.employee.service;
+
+import com.skypro.employee.model.Employee;
+import com.skypro.employee.record.EmployeeRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+//здесь хранятся сотрудники
+@Service
+public class EmployeeService {
+    private final Map<Integer, Employee> employees = new HashMap<>();
+
+
+    public Collection<Employee> getAllEmployees() {
+        return this.employees.values();
+    }
+
+    public Employee addEmployee(EmployeeRequest employeeRequest) {
+        if (employeeRequest.getFirstName() == null || employeeRequest.getLastName() == null) {
+            throw new IllegalArgumentException("Employee name should be set");
+        }
+        Employee employee = new Employee(employeeRequest.getFirstName(), employeeRequest.getLastName(), employeeRequest.getDepartment(), employeeRequest.getSalary());
+
+        this.employees.put(employee.getId(), employee);
+        return employee;
+    }
+
+    public int getSalarySum() {
+        return employees.values().stream()
+                .mapToInt(Employee::getSalary)
+                .sum();
+    }
+
+    public int getSalaryMax() {
+        return employees.values().stream()
+                .mapToInt(Employee::getSalary)
+                .max().getAsInt();
+    }
+
+    public int getSalaryMin() {
+        return employees.values().stream()
+                .mapToInt(Employee::getSalary)
+                .min().getAsInt();
+    }
+
+    public Collection<Employee> getEmployeeBiggerThanAverage() {
+        double opt = employees.values().stream()
+                .mapToInt(Employee::getSalary)
+                .average().getAsDouble();
+        return employees.values().stream()
+                .filter(employee -> employee.getSalary() > opt).collect(Collectors.toList());
+    }
+}
